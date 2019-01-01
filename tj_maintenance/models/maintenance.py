c8859cd00ca9dcf1e2f5352b2377d41ff191df00
lijianming <lijianming@163.com>
2019-01-01 10:06:20 +0800

    维修申请

--- tj_maintenance/models/maintenance.py
+++ tj_maintenance/models/maintenance.py
@@ -13,7 +13,6 @@ class TJMaintenanceEquipment(models.Model):
 
     equipment_parts_ids = fields.One2many('equipment.parts', 'maintenance_equipment_id', '部件')
     attachment_ids = fields.One2many('ir.attachment', 'res_id', domain=[('res_model', '=', 'maintenance.equipment')], string='附件')
-
     @api.multi
     def action_get_attachment_view(self):
         res = self.env['ir.actions.act_window'].for_xml_id('base', 'action_attachment')
@@ -27,8 +26,10 @@ class TJMaintenanceRequest(models.Model):
     _description = "维护申请"
 
     attachment_ids = fields.One2many('ir.attachment', 'res_id', domain=[('res_model', '=', 'maintenance.request')],string='附件')
-    equipment_parts_id = fields.Many2one('equipment.parts','部件')
-
+    equipment_parts_id = fields.Many2one('equipment.parts', '设备部位')
+    equipment_serial = fields.Char("设备序列", related='equipment_id.serial_no')
+    owner_user_num = fields.Integer("工号")
+    owner_user_team_id = fields.Char("工种")
     @api.multi
     def action_get_attachment_view(self):
         res = self.env['ir.actions.act_window'].for_xml_id('base', 'action_attachment')
@@ -37,8 +38,8 @@ class TJMaintenanceRequest(models.Model):
         return res
 
 class EquipmentParts(models.Model):
-    _name = "equipment.parts"
-    _description = "设备部位"
+    _name = "equipment.parts"  # 设备-设备部位-设备部件
+    _description = "设备部件"
 
-    name = fields.Char('设备部位名称')
+    name = fields.Char('设备部件名称')
     maintenance_equipment_id = fields.Many2one('maintenance.equipment','机台')
