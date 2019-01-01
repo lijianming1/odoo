ef540e6c03f9d2d265882742d243a0d9da26c567
Ivan <www.yifugui.w@gmail.com>
2019-01-01 14:21:19 +0800

    [F0004]ADD:维护申请的维修方和申请方

--- tj_maintenance/models/maintenance.py
+++ tj_maintenance/models/maintenance.py
@@ -13,7 +13,6 @@ class TJMaintenanceEquipment(models.Model):
 
     equipment_parts_ids = fields.One2many('equipment.parts', 'maintenance_equipment_id', '部件')
     attachment_ids = fields.One2many('ir.attachment', 'res_id', domain=[('res_model', '=', 'maintenance.equipment')], string='附件')
-
     @api.multi
     def action_get_attachment_view(self):
         res = self.env['ir.actions.act_window'].for_xml_id('base', 'action_attachment')
@@ -27,7 +26,28 @@ class TJMaintenanceRequest(models.Model):
     _description = "维护申请"
 
     attachment_ids = fields.One2many('ir.attachment', 'res_id', domain=[('res_model', '=', 'maintenance.request')],string='附件')
-    equipment_parts_id = fields.Many2one('equipment.parts','部件')
+    equipment_parts_id = fields.Many2one('equipment.parts', '设备部位')
+    equipment_serial = fields.Char("设备序列", related='equipment_id.serial_no')
+    owner_user_num = fields.Char("工号")
+    owner_user_team_id = fields.Char("工种")
+    owner_user_id = fields.Many2one('res.users', default=False)
+    receiving_user = fields.Many2one('res.users')
+    receiving_user_num = fields.Char("工号")
+    receiving_user_team_id = fields.Char("工种")
+    fault_cause_analysis = fields.Text("故障原因分析")
+    receiving_date = fields.Datetime("接收时间")
+    fault_type = fields.Many2one('fault.type', "故障类型")
+    processing_way = fields.Many2one('processing.way', "处理方式")
+    receiving_description = fields.Text("维修方备注")
+
+    @api.multi
+    def write(self, vals):
+        res = super(TJMaintenanceRequest, self).write(vals)
+        if self.stage_id.sequence == 2 and 'stage_id' in vals:
+            self.write({'receiving_date': fields.Datetime.now()})
+        if self.stage_id.sequence == 1 and 'stage_id' in vals and self.receiving_date:
+            raise UserError('正在进行的维护单不可重复提交！')
+        return res
 
     @api.multi
     def action_get_attachment_view(self):
@@ -38,7 +58,19 @@ class TJMaintenanceRequest(models.Model):
 
 class EquipmentParts(models.Model):
     _name = "equipment.parts"
-    _description = "设备部位"
+    _description = "机台部件"
 
-    name = fields.Char('设备部位名称')
+    name = fields.Char('机台部件名称')
     maintenance_equipment_id = fields.Many2one('maintenance.equipment','机台')
+
+class FaulType(models.Model):
+    _name = "fault.type"
+    _description = "故障类型"
+
+    name = fields.Char('故障类型名称')
+
+class ProcessingWay(models.Model):
+    _name = "processing.way"
+    _description = "处理方式"
+
+    name = fields.Char('处理方式名称')
